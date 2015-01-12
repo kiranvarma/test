c51417389e8a89e0a3092522d1aa1e602bbabe69
kiranvarma <sravanpeddi@Sravans-MacBook-Pro.local>
2015-01-12 22:22:59 +0530

    initial

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Sravan Peddi on 12/01/15.
+//  Copyright (c) 2015 peddi.sravan@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Sravan Peddi on 12/01/15.
+//  Copyright (c) 2015 peddi.sravan@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
